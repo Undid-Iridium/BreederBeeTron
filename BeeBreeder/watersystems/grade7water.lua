d03cf70b42b659c9ed29df72075b7d504f49b6e4
Undid Iridium <FrancisXIrizarry@gmail.com>
2026-04-23 04:54:26 -0400

    Almost there

--- BeeBreeder/watersystems/grade7water.lua
+++ BeeBreeder/watersystems/grade7water.lua
@@ -43,18 +43,51 @@ local main = thread.create(function()
         local lens_to_use = 2
         local prev = lens_to_use-1
 
-        local _, _, side, oldValue, newValue = event.pull("redstone_changed")
-        if newValue > 0 then
-            
+        local _, _, side, oldValue, new_value = event.pull("redstone_changed")
+        if new_value == 0 then
+            -- Super coolant 10000L
+        else
             print_d(" : Signal received on side: " .. side)
-            print_d(" : Signal received on side: " .. newValue)
-            local temp_val = tonumber(newValue)
+            print_d(" : Signal received on side: " .. new_value)
+            local temp_val = tonumber(new_value)
             print_d(" : Signal received on side temp: " .. temp_val)
-            print_d(" : Signal received on side: " .. temp_val & 1)
-            print_d(" : Signal received on side: " .. temp_val & 2)
-            print_d(" : Signal received on side: " .. temp_val & 3)
-            print_d(" : Signal received on side: " .. temp_val & 4)
+            local bruh = temp_val & (1 << 0)
+            print("hmm")
+            print_d(" : Signal received on side: " .. bruh)
+            print_d(" : Signal received on side: " .. (temp_val & 1))
+            print_d(" : Signal received on side: " .. (temp_val & 2))
+            print_d(" : Signal received on side: " .. (temp_val & 4))
+            print_d(" : Signal received on side: " .. (temp_val & 3))
+
+            if temp_val & (1 << 3) ~= 0 then -- bit 4 ( :( 4-1 ))
+                goto continue
+            elseif temp_val & (1 << 0) ~= 0 then
+                -- Take 2 bits for value
+                local low_bit = temp_val & (1 << 1)
+                local high_bit =  temp_val & (1 << 2)
+                local total = 0
+                total = total | (low_bit << 0)
+                total = total | (high_bit << 1)
+
+                -- Result will be 00 01 10 11 (there are 0's before the 2 but ignore)
+
+                if(total == 0) then
+                    -- Helium 10000L
+                elseif total == 1 then
+                    -- Neon 7500L
+                elseif total == 2 then
+                    -- Krypton 5000L
+                elseif total == 3 then
+                    -- Xenon 2500L
+                end                
+                
+            elseif temp_val & (1 << 2) ~= 0 then
+                -- Neutronium 4608L
+            elseif temp_val & (1 << 1) ~= 0 then
+                --  1440 L of any Molten Superconductor Base of at least UV tier
+            end
         end
+        ::continue::
     end
   end)
 
